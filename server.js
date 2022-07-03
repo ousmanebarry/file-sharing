251b7345aa70bb40210a2368391c880b1d9deab7
Ousmane Barry <ppobarry@gmail.com>
2022-07-03 01:38:27 -0400

    check for files with passwords

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ mongoose.connect(process.env.DATABASE_URL);
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -34,8 +35,28 @@ app.post('/upload', upload.single('file'), async (req, res) => {
   res.render('index', { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
 
-app.get('/file/:id', (req, res) => {
-  res.send(req.params.id);
-});
+app.route('/file/:id').get(handleDownload).post(handleDownload);
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id);
+
+  if (file.password != null && file.password !== '') {
+    if (req.body.password == null) {
+      res.render('password');
+      return;
+    }
+
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render('password', { error: true });
+      return;
+    }
+  }
+
+  file.downloadCount++;
+  await file.save();
+  console.log(file.downloadCount);
+
+  res.download(file.path, file.originalName);
+}
 
 app.listen(process.env.PORT);
