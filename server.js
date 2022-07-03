a80ddca4819ce437e10c5e4dc5ac3bd2d1ad5ce3
Ousmane Barry <ppobarry@gmail.com>
2022-07-03 00:12:48 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const multer = require('multer');
+
 const app = express();
+const upload = multer({ dest: 'uploads/' });
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index.ejs');
   console.log(req);
+});
+
+app.post('/upload', (req, res) => {
+  res.send('hi');
 });
 
 app.listen(3000);
