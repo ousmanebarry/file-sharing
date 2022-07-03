c952d22ca3342834a473f0b9def4d85ec603577a
Ousmane Barry <ppobarry@gmail.com>
2022-07-02 22:52:46 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+  console.log(req);
 });
 
 app.listen(3000);
